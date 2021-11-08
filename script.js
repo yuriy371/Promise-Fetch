b2e8c0bdc4c8959dbd39a062109666159febca42
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-11-08 10:30:02 +0400

    lesson25

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log(15);
+let user = fetch("db.json")
+
+let getData = () => {
+    user
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data);
+            sendData(data)
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+let sendData = (data) => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((response) => response.json())
+        .then((json) => console.log(json))
+        .catch(error => console.log(error))
+}
+getData()
